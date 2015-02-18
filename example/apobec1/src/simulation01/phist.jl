377c9d0385c835c7469dace594c0ffda1e9f4216
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-02-18 12:01:34 +0000

    First attempt to simulate from prior of editing rates and plot them

--- example/apobec1/src/simulation01/phist.jl
+++ example/apobec1/src/simulation01/phist.jl
@@ -5,13 +5,27 @@ using CellwiseEditingDifferentiation
 using Distributions
 using Gadfly
 
+hyperpars = Dict{Symbol, Any}(:λ=>fill(300.0, nsites))
+
+prior = Dict{Symbol, Any}(:v=>Function[(m::Float64, v::Float64, a::Float64, b::Float64)->
+  vpcprior(m, v, a, b, hyperpars[:λ][i]) for i in 1:nsites])
+
+target = Dict{Symbol, Any}(:v=>Array(Function, nsites))
 for i in 1:nsites
-  # colors = distinguishable_colors(2)
-  colors = Dict{Symbol, Color.RGB{Float64}}(:posterior=>color("red"))
+  target[:v][i] = function (v::Float64)
+    a, b = beta_pars_from_mv(data[:m][i], v)
+    try
+      exp(prior[:v][i](data[:m][i], v, a, b))
+    catch
+      0.
+    end
+  end
+end
 
-  np = 10000
+support = Dict{Symbol, Any}(:v=>Any[0.0001:0.0001:0.2458, 0.0001:0.0001:0.2476])
 
-  layers = Layer[]
+for i in 1:nsites
+  np = 100000
 
   vposterior = vec(readdlm(joinpath(OUTDIR, @sprintf("vchain_%s_site%02d.txt", string(simulationid), i))))
 
@@ -25,6 +39,27 @@ for i in 1:nsites
   println("Mean of pposterior for site $i = $(mean(pposterior))")
   println("Var of pposterior for site $i = $(var(pposterior))")
 
+  u = 0.25*rand(Uniform(0, 1), np)
+  weight = map(target[:v][i], u)
+  weight = weight/sum(weight)
+  index = rand(Categorical(weight), np)
+  vprior = u[index]
+
+  pprior = Array(Float64, np)
+
+  for j in 1:np
+    a, b = beta_pars_from_mv(data[:m][i], vprior[i])
+    pprior[j] = rand(Beta(a, b))
+  end
+
+  println("Mean of pprior for site $i = $(mean(pprior))")
+  println("Var of pprior for site $i = $(var(pprior))")
+
+  # colors = distinguishable_colors(2)
+  colors = Dict{Symbol, Color.RGB{Float64}}(:posterior=>color("red"), :prior=>color("blue"))
+
+  layers = Layer[]
+
   push!(layers, layer(
     x=pposterior,
     Stat.histogram(bincount=50, density=true),
@@ -32,11 +67,18 @@ for i in 1:nsites
     Theme(default_color=colors[:posterior])
   )[1])
 
+  push!(layers, layer(
+    x=pprior,
+    Stat.histogram(bincount=50, density=true),
+    Geom.line,
+    Theme(default_color=colors[:prior])
+  )[1])
+
   vplot = plot(
     layers,
     Guide.xlabel("p<sub>$i</sub>"),
     Guide.title("Histogram of p<sub>$i</sub>"),
-    #Guide.manual_color_key("Distribution", [string(k) for k in keys(colors)], [c for c in values(colors)])
+    Guide.manual_color_key("Distribution", [string(k) for k in keys(colors)], [c for c in values(colors)])
   )
 
   draw(PDF(joinpath(OUTDIR, @sprintf("phist_%s_site%02d.pdf", string(simulationid), i)), 4inch, 3inch), vplot)
--- example/apobec1/src/simulation01/vhist.jl
+++ example/apobec1/src/simulation01/vhist.jl
@@ -22,29 +22,29 @@ support = Dict{Symbol, Any}(:v=>Any[0.0001:0.0001:0.2458, 0.0001:0.0001:0.2476])
 vprior_xmin = fill(0., nsites)
 vprior_xmax = fill(0.03, nsites)
 vprior_ymin = fill(0., nsites)
-vprior_ymax = fill(200., nsites)
+vprior_ymax = fill(400., nsites)
 
 for i in 1:nsites
+  vpdf = Float64[target[:v][i](x) for x in support[:v][i]]
+  c = quadgk(target[:v][i], support[:v][i][1], support[:v][i][end])
+
   # colors = distinguishable_colors(2)
   colors = Dict{Symbol, Color.RGB{Float64}}(:posterior=>color("red"), :prior=>color("blue"))
 
   layers = Layer[]
-
-  vpdf = Float64[target[:v][i](x) for x in support[:v][i]]
-  c = quadgk(target[:v][i], support[:v][i][1], support[:v][i][end])
-
-  push!(layers, layer(
-    x=collect(support[:v][i]),
-    y=vpdf/c[1],
-    Geom.line,
-    Theme(default_color=colors[:prior])
-  )[1])
 
   push!(layers, layer(
     x=vec(readdlm(joinpath(OUTDIR, @sprintf("vchain_%s_site%02d.txt", string(simulationid), i)))),
     Stat.histogram(bincount=50, density=true),
     Geom.line,
     Theme(default_color=colors[:posterior])
+  )[1])
+
+  push!(layers, layer(
+    x=collect(support[:v][i]),
+    y=vpdf/c[1],
+    Geom.line,
+    Theme(default_color=colors[:prior])
   )[1])
 
   vplot = plot(
