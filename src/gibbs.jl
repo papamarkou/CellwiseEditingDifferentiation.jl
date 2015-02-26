94f394746d5fbb215f7191e06d360588cbd03d19
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-02-26 15:39:00 +0000

    WIP: small breakage while setting model for missing data

--- src/gibbs.jl
+++ src/gibbs.jl
@@ -1,14 +1,22 @@
 ### This is not a generically coded Metropolis-within-Gibbs sampler, it is tailored to the problem at hand
 
 function gibbs(data::Dict{Symbol, Any}, init::Dict{Symbol, Any}, job)
-  nsites, ncells = size(data[:coverage])
-  @assert length(data[:m]) == nsites "length(data[:m]) must be equal to size(data[:coverage], 1)"
-  @assert size(data[:edited]) == (nsites, ncells) "size(data[:edited]) must be equal to size(data[:coverage])"
+  nsites = size(data[:coverage], 1)
 
-  nsamples = length(job[:gibbs].r)
+  cells = Dict{Int, Vector{Int}}()
+  for i in 1:nsites
+    cells[i] = find(data[:coverage][i, :] .!= 0)
+  end
+
+  ncells = Dict{Int, Int}()
+  for i in 1:nsites
+    ncells[i] = length(cells[i])
+  end
+
+  nmc = length(job[:gibbs].r)
 
   mcsample = Dict{Symbol, Any}(:w=>init[:w], :p=>init[:p])
-  mcchain = Dict{Symbol, Any}(:w=>Array(Float64, nsamples, nsites), :p=>Array(Float64, nsamples, nsites, ncells))
+  mcchain = Dict{Symbol, Any}(:w=>Array(Float64, nmc, nsites), :p=>Array(Any, nmc))
 
   counter::Int = 1
   tic()
@@ -18,18 +26,18 @@ function gibbs(data::Dict{Symbol, Any}, init::Dict{Symbol, Any}, job)
     tic()
 
     for i in 1:nsites
-      wchain = Lora.run(job[:w][i](vec(mcsample[:p][i, :]), [mcsample[:w][i]]))
+      wchain = Lora.run(job[:w][i](mcsample[:p][i], [mcsample[:w][i]]))
       mcsample[:w][i] = wchain.samples[end, :][1]
       @printf("  RAM acceptance rate for site %02d: %.2f%%\n", i, round(acceptance(wchain), 2))
 
-      for j in 1:ncells
-        mcsample[:p][i, j] = run(job[:p][i, j](mcsample[:w][i]))
+      for j in 1:ncells[i]
+        mcsample[:p][i][j] = run(job[:p][i][j](mcsample[:w][i]))
       end
     end
 
     if in(k, job[:gibbs].r)
       mcchain[:w][counter, :] = mcsample[:w]
-      mcchain[:p][counter, :, :] = mcsample[:p]
+      mcchain[:p][counter] = mcsample[:p]
       counter += 1
     end
 
