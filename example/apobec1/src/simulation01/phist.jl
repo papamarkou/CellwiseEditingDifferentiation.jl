90ac9b43d1a9754f3d666d7ead31eaf9d739087d
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-02-26 11:52:42 +0000

    Towards fixing phist after logit transform correction

--- example/apobec1/src/simulation01/phist.jl
+++ example/apobec1/src/simulation01/phist.jl
@@ -4,25 +4,23 @@ using CellwiseEditingDifferentiation
 # using Color
 using Distributions
 using Gadfly
+using Lora
 
 hyperpars = Dict{Symbol, Any}(:λ=>fill(300.0, nsites))
 
 prior = Dict{Symbol, Any}(:v=>Function[(m::Float64, v::Float64, a::Float64, b::Float64)->
   vpcprior(m, v, a, b, hyperpars[:λ][i]) for i in 1:nsites])
 
-target = Dict{Symbol, Any}(:v=>Array(Function, nsites))
+prior_target = Dict{Symbol, Any}(:w=>Array(Function, nsites))
 for i in 1:nsites
-  target[:v][i] = function (v::Float64)
+  prior_target[:w][i] = function (w::Vector{Float64})
+    v = data[:m][i]*(1-data[:m][i])*inv_logit(w[1])
     a, b = beta_pars_from_mv(data[:m][i], v)
-    try
-      exp(prior[:v][i](data[:m][i], v, a, b))
-    catch
-      0.
-    end
+    prior[:v][i](data[:m][i], v, a, b)+logdvdw(w[1], data[:m][i])
   end
 end
 
-support = Dict{Symbol, Any}(:v=>fill(0.0001:0.0001:0.9999*data[:m][i]*(1-data[:m][i]), nsites))
+prior_init = Dict{Symbol, Any}(:w=>fill(0.0, nsites))
 
 for i in 1:nsites
   np = 100000
@@ -39,17 +37,17 @@ for i in 1:nsites
   println("Mean of pposterior for site $i = $(mean(pposterior))")
   println("Var of pposterior for site $i = $(var(pposterior))")
 
-  u = data[:m][i]*(1-data[:m][i])*rand(Uniform(0, 1), np)
-  weight = map(target[:v][i], u)
-  weight = weight/sum(weight)
-  index = rand(Categorical(weight), np)
-  vprior = u[index]
+  mcmodel = model(prior_target[:w][i], init=[prior_init[:w][i]])
+  mcsampler = RAM()
+  mcrunner = SerialMC(nsteps=int(1.01*np), burnin=int(0.1*np))
+  mcchain = run(mcmodel, mcsampler, mcrunner)
+  mcchain = map(w->data[:m][i]*(1-data[:m][i])*inv_logit(w), vec(mcchain.samples))
 
   pprior = Array(Float64, np)
 
-  for j in 1:np
-    a, b = beta_pars_from_mv(data[:m][i], vprior[i])
-    pprior[j] = rand(Beta(a, b))
+  for j in 1:ncells
+    a, b = beta_pars_from_mv(data[:m][i], rand(mcchain))
+    pprior[j] = rand(Beta(a+data[:edited][i, j], b+data[:coverage][i, j]-data[:edited][i, j]))
   end
 
   println("Mean of pprior for site $i = $(mean(pprior))")
