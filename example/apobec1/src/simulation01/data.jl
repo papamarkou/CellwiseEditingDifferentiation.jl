78079c4675f6344ac7688594ec0d662c471879c0
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2015-02-26 13:00:07 +0000

    WIP: Started changing code to deal with missing data (i.e. with cells with zero coverage)

--- example/apobec1/src/simulation01/data.jl
+++ example/apobec1/src/simulation01/data.jl
@@ -9,15 +9,26 @@ OUTDIR = joinpath("../../output", string(simulationid))
 
 DATAFILE = joinpath(DATADIR, "apobec1.txt")
 
-sites = DataFrame(chr = ["chr2", "chr16"], coord = [122152902, 84954513])
+df = readtable(DATAFILE, separator = '\t')
+
+sites = DataFrame(chr = ["chr10", "chr11"], coord = [57515985, 100476998])
+# sites = DataFrame(chr = ["chr2", "chr16"], coord = [122152902, 84954513])
 # sites = DataFrame(chr = ["chrX", "chr4", "chr5"], coord = [109162174, 6395365, 104440840])
 nsites = nrow(sites)
 
-df = readtable(DATAFILE, separator = '\t')
 df = df[intersect(findin(df[:chr], sites[:chr]), findin(df[:coord], sites[:coord])), :]
 
 data = Dict{Symbol, Any}(:m=>df[:, :Bulk_er],
                          :coverage=>float(array(df[:, 5:2:51])),
                          :rate=>float(array(df[:, 6:2:52])))
 data[:edited] = float(data[:rate].*data[:coverage])
-ncells = size(data[:coverage], 2)
+
+nonmissing_data = Dict{Int, Vector{Int}}()
+for i in 1:nsites
+  nonmissing_data[i] = find(data[:coverage][i, :] .!= 0)
+end
+
+ncells = Dict{Int, int}()
+for i in 1:nsites
+  ncells[i] = length(nonmissing_data[i])
+end
