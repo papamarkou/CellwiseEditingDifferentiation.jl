fbb9dfc583fbdd644cd660cac2cda248d27fb472
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-05-30 22:00:26 +0100

    Minor tweaks in example with artificial data

--- examples/artificial_data/gibbs_sampling.jl
+++ examples/artificial_data/gibbs_sampling.jl
@@ -19,24 +19,20 @@ coverage = Data(:coverage)
 edited = Data(:edited)
 rate = Data(:rate)
 
-w = BasicContUnvParameter(
-  :w,
-  logtarget = function (w::Float64, v::Vector)
-    θ = v[2]*(1-v[2])*inv_logit(w)
-    a, b = beta_pars_from_mv(v[2], θ)
-    mapreduce(x -> logpdf(Beta(a, b), x), +, v[7])+vpcprior(v[2], θ, a, b, v[1])+logdvdw(w, v[2])
-  end,
-  nkeys=7
-)
+function wlogtarget(w::Float64, s::Vector)
+  v = s[2]*(1-s[2])*inv_logit(w)
+  a, b = beta_pars_from_mv(s[2], v)
+  mapreduce(x -> logpdf(Beta(a, b), x), +, s[7])+vpcprior(s[2], v, a, b, s[1])+logdvdw(w, s[2])
+end
 
-p = BasicContMuvParameter(
-  :p,
-  setpdf = function (p::Vector{Float64}, v::Vector)
-    a, b = beta_pars_from_mv(v[2], v[2]*(1-v[2])*inv_logit(v[6]))
-    MvBeta([Beta(a+v[4][i], b+v[3][i]-v[4][i]) for i in 1:ncells])
-  end,
-  nkeys=7
-)
+w = BasicContUnvParameter(:w, logtarget=wlogtarget, nkeys=7)
+
+function psetpdf(p::Vector{Float64}, s::Vector)
+  a, b = beta_pars_from_mv(s[2], s[2]*(1-s[2])*inv_logit(s[6]))
+  MvBeta([Beta(a+s[4][i], b+s[3][i]-s[4][i]) for i in 1:ncells])
+end
+
+p = BasicContMuvParameter(:p, setpdf=psetpdf, nkeys=7)
 
 model = GenericModel(
   [λ, μ, coverage, edited, rate, w, p],
@@ -44,21 +40,21 @@ model = GenericModel(
   isindexed=false
 )
 
-v0 = Dict(
+init = Dict(
   :λ => 10.,
   :μ => 0.5,
   :coverage => fill(10.0, 20),
   :edited => rand([1., 2., 3., 8., 9.], 20),
-  :w => 0.,
-  :p => fill(0.1, ncells)
+  :w => 0.
 )
-v0[:rate] = float(v0[:edited]./v0[:coverage])
+init[:rate] = float(init[:edited]./init[:coverage])
+init[:p] = rand(psetpdf(fill(NaN, ncells), [init[k] for k in [:λ, :μ, :coverage, :edited, :rate, :w]]))
   
 wjob = BasicMCJob(
   model,
   RAM(),
   BasicMCRange(nsteps=100, burnin=10),
-  v0;
+  init;
   pindex=6,
   resetpstate=false,
   verbose=false
@@ -68,7 +64,7 @@ mcrange = BasicMCRange(nsteps=1100, burnin=100)
 
 outopts = [Dict(:monitor => [:value]), Dict(:monitor => [:value])]
 
-gibbsjob = GibbsJob(model, Dict(:w => wjob), mcrange, v0)
+gibbsjob = GibbsJob(model, Dict(:w => wjob), mcrange, init)
   
 @time run(gibbsjob)
 
@@ -76,5 +72,7 @@ output(gibbsjob)
 
 chains = Dict(gibbsjob)
 
-θ = map(x -> v0[:μ]*(1-v0[:μ])*inv_logit(x), chains[:w].value)
+v = map(x -> init[:μ]*(1-init[:μ])*inv_logit(x), chains[:w].value)
 
+writecsv("pchains.csv", chains[:p].value)  
+writecsv("vchain.csv", v)
